136d3460f7416455a6f822fb377d9fda387fe1bf
Olivier <oliviervandeneede@hotmail.com>
2016-07-24 16:09:02 +0200

    fixed bootstrap https

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     
     <title>@yield("titel")</title>
 
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
     @yield("header")
 
