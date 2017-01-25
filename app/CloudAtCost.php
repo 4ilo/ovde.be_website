6ae67d33e2551c01922df3e963fc1116ca7a31a1
Olivier <oliviervandeneede@hotmail.com>
2017-01-25 17:41:28 +0100

    fix server status

--- app/CloudAtCost.php
+++ app/CloudAtCost.php
@@ -1,119 +1,156 @@
 <?php
+
+namespace App;
+
+use GuzzleHttp\Client;
+use App\Exceptions\CacHostnameNotFoundException;
+use App\Exceptions\CloudAtCostLoginFailException;
+
+
+class CloudAtCost
+{
+
+    private $client;
+    private $server;
+    private $data;
+
+    private $apiKey;
+    private $apiLogin;
+
     /**
-     * Created by PhpStorm.
-     * User: Olivier
-     * Date: 11/01/16
-     * Time: 16:58
+     *  CloudAtCost constructor.
      */
-    
-    namespace App;
-
-    use App\CACApi;
-    use App\Exceptions\CloudAtCostLoginFailException;
-
-    class CloudAtCost
+    public function __construct($apiKey, $apiLogin)
     {
+        $this->client = new Client();
 
-        private $verbinding;
-        private $server;
-
-        /**
-         * CloudAtCost constructor.
-         */
-        public function __construct()
-        {
-            $this->verbinding = new CACApi(array(
-                "key" => env("CAC_KEY"),
-                "login" => env("CAC_LOGIN")
-            ));
-        }
-
-        /**
-         * De server ophalen in de cac connectie
-         * @param $id
-         * @throws CloudAtCostLoginFailException
-         */
-        public function server($id)
-        {
-            $temp = $this->verbinding->getServers();
-            if (isset($temp["status"]) || $temp == null)
-            {
-                throw new CloudAtCostLoginFailException;
-            }
-            $this->server = $temp[$id];
-        }
-
-        /**
-         * Het gebruikte percentage ram ophahalen
-         * @return int
-         */
-        public function ramUsage()
-        {
-            $totaal = $this->server["ram"];
-            $gebruik = $this->server["ramusage"];
-            return intval($gebruik * 100 / $totaal);
-        }
-
-        /**
-         * Het gebruikte percentage hdd ophahalen
-         * @return int
-         */
-        public function hddUsage()
-        {
-            $totaal = $this->server["storage"];
-            $gebruik = $this->server["hdusage"];
-            return intval($gebruik * 100 / $totaal);
-        }
-
-        /**
-         * Het gebruikte percentage cpu ophahalen
-         * @return int
-         */
-        public function cpuUsage()
-        {
-            $totaal = $this->server["cpu"]* (266.66);
-            $gebruik = $this->server["cpuusage"];
-            return intval($gebruik * 100 / $totaal);
-        }
-
-        /**
-         * De juiste klasse ophalen voor de progressBar
-         * @param $soort
-         * @return string
-         */
-        public function statusColor($soort)
-        {
-            $percentage = 0;
-
-            switch($soort)
-            {
-                case "ram":
-                   $percentage = $this->ramUsage();
-                    break;
-                case "cpu":
-                    $percentage = $this->cpuUsage();
-                    break;
-                case "hdd":
-                    $percentage = $this->hddUsage();
-                    break;
-            }
-
-            if ($percentage < 25)
-            {
-                return "progress-bar-success";
-            }
-            if ($percentage < 50)
-            {
-                return "progress-bar-info";
-            }
-            if ($percentage < 75)
-            {
-                return "progress-bar-warning";
-            }
-            if ($percentage < 100)
-            {
-                return "progress-bar-danger";
-            }
-
-        }
+        $this->apiKey = $apiKey;
+        $this->apiLogin = $apiLogin;
     }
+
+    /**
+     * Gets the data from the cloud at cost api
+     *
+     * @throws     \App\Exceptions\CloudAtCostLoginFailException  (description)
+     */
+    public function getData()
+    {
+        $requestData = $this->client->get("https://panel.cloudatcost.com/api/v1/listservers.php?key=".$this->apiKey."&login=".$this->apiLogin);
+
+        if( isset($requestData->json()["error"]) )
+            throw new CloudAtCostLoginFailException; 
+
+        $this->data = $requestData->json()["data"]; 
+    }
+
+    /**
+     *  De server ophalen in de cac connectie by id
+     * 
+     *  @param $id
+     *  @throws CloudAtCostLoginFailException
+     */
+    public function Server($id)
+    {
+        $this->getdata();
+
+        $this->server = $this->data[$id];
+    }
+
+    /**
+     * Select a server by its hostname
+     *
+     * @param      String  $hostname  The hostname
+     */
+    public function selectServer($hostname)
+    {
+        $this->getdata();
+
+        foreach ($this->data as $server) 
+        {
+            if($server["hostname"] === $hostname)
+            {
+                $this->server = $server;
+                return;
+            }
+        }
+
+        throw new CacHostnameNotFoundException;
+    }
+
+    /**
+     *  Het gebruikte percentage ram ophahalen
+     *  
+     *  @return int
+     */
+    public function ramUsage()
+    {
+        $totaal = $this->server["ram"];
+        $gebruik = $this->server["ramusage"];
+        return intval($gebruik * 100 / $totaal);
+    }
+
+    /**
+     *  Het gebruikte percentage hdd ophahalen
+     *  
+     *  @return int
+     */
+    public function hddUsage()
+    {
+        $totaal = $this->server["storage"];
+        $gebruik = $this->server["hdusage"];
+        return intval($gebruik * 100 / $totaal);
+    }
+
+    /**
+     *  Het gebruikte percentage cpu ophahalen
+     *  
+     *  @return int
+     */
+    public function cpuUsage()
+    {
+        $totaal = $this->server["cpu"] * (266.66);
+        $gebruik = $this->server["cpuusage"];
+        return intval($gebruik * 100 / $totaal);
+    }
+
+    /**
+     *  De juiste klasse ophalen voor de progressBar
+     *  
+     *  @return string
+     */
+    public function statusColor($soort)
+    {
+        $percentage = 0;
+
+        switch($soort)
+        {
+            case "ram":
+               $percentage = $this->ramUsage();
+                break;
+            case "cpu":
+                $percentage = $this->cpuUsage();
+                break;
+            case "hdd":
+                $percentage = $this->hddUsage();
+                break;
+        }
+
+        if ($percentage < 25)
+        {
+            return "progress-bar-success";
+        }
+        if ($percentage < 50)
+        {
+            return "progress-bar-info";
+        }
+        if ($percentage < 75)
+        {
+            return "progress-bar-warning";
+        }
+        if ($percentage < 100)
+        {
+            return "progress-bar-danger";
+        }
+
+    }
+}
--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -1,110 +1,111 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use fdisotto\CACApi;
-    use GuzzleHttp\Client;
-    use App\CloudAtCost;
-    use Illuminate\Support\Facades\Mail;
-    use App\Http\Requests\contactRequest;
-    use ReCaptcha\ReCaptcha;
+use App\CloudAtCost;
+use GuzzleHttp\Client;
+use ReCaptcha\ReCaptcha;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\contactRequest;
 
-    class pagesController extends Controller
+class pagesController extends Controller
+{
+    /**
+     * De homepage
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function home()
     {
-        /**
-         * De homepage
-         * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-         */
-        public function home()
-        {
-            return view("paginas.home");
-        }
-
-        /**
-         * Over olivier pagina
-         * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-         */
-        public function over()
-        {
-            return view("paginas.over");
-        }
-
-        /**
-         * Projecten pagina
-         * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-         */
-        public function projecten()
-        {
-            return view("paginas.projecten");
-        }
-
-        /**
-         * De contact pagina
-         * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-         */
-        public function contact()
-        {
-            return view("paginas.contact");
-        }
-
-        /**
-         * Deze method stuurt de mail van het contactformulier
-         * @param contactRequest $request
-         * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-         */
-        public function sendMail(contactRequest $request)
-        {
-            if( !$this->checkCaptcha($request) )
-            {
-                session()->flash("flash_message","reCAPTCHA is niet ingevuld");
-                return redirect()->back()->withInput();
-            }
-                // Hier is de captcha gevalideerd
-
-            Mail::send('emails.contact', ["request" => $request], function ($message) {
-                $message->from('contact@ovde.be', 'Ovde.be');
-                $message->to('oliviervandeneede@hotmail.com')->subject("Contactformulier");
-            });
-
-            session()->flash("flash_message","Email is verzonden.");
-            return redirect("contact");
-        }
-
-        /**
-         * Functie om cpatcha te testen
-         * @param $request
-         * @return bool
-         */
-        private function checkCaptcha($request)
-        {
-            if(env("RECAPTCHA_BYPASS",false))
-            {
-                return true;
-            }
-            $response = $request->input("g-recaptcha-response");
-            $remoteip = $_SERVER['REMOTE_ADDR'];
-            $secret   = env('RE_CAP_SECRET');
-
-            $recaptcha = new ReCaptcha($secret);
-            $resp = $recaptcha->verify($response,$remoteip);
-
-            if($resp->isSuccess())
-            {
-                return true;
-            }
-            return false;
-        }
-
-
-        /**
-         * Een statuspagina met ram,cpu en hdd usage
-         * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-         * @throws \App\Exceptions\CloudAtCostLoginFailException
-         */
-        public function status()
-        {
-            $vps = new CloudAtCost();
-            $vps->server(0);
-            return view("paginas.status",compact("vps"));
-        }
+        return view("paginas.home");
     }
+
+    /**
+     * Over olivier pagina
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function over()
+    {
+        return view("paginas.over");
+    }
+
+    /**
+     * Projecten pagina
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function projecten()
+    {
+        return view("paginas.projecten");
+    }
+
+    /**
+     * De contact pagina
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function contact()
+    {
+        return view("paginas.contact");
+    }
+
+    /**
+     *  Deze method stuurt de mail van het contactformulier
+     *  @param contactRequest $request
+     *  @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function sendMail(contactRequest $request)
+    {
+        if( !$this->checkCaptcha($request) )
+        {
+            session()->flash("flash_message","reCAPTCHA is niet ingevuld");
+            return redirect()->back()->withInput();
+        }
+            // Hier is de captcha gevalideerd
+
+        Mail::send('emails.contact', ["request" => $request], function ($message) {
+            $message->from('contact@ovde.be', 'Ovde.be');
+            $message->to('oliviervandeneede@hotmail.com')->subject("Contactformulier");
+        });
+
+        session()->flash("flash_message","Email is verzonden.");
+        return redirect("contact");
+    }
+
+    /**
+     * Functie om cpatcha te testen
+     * @param $request
+     * @return bool
+     */
+    private function checkCaptcha($request)
+    {
+        if(env("RECAPTCHA_BYPASS",false))
+        {
+            return true;
+        }
+        
+        $response = $request->input("g-recaptcha-response");
+        $remoteip = $_SERVER['REMOTE_ADDR'];
+        $secret   = env('RE_CAP_SECRET');
+
+        $recaptcha = new ReCaptcha($secret);
+        $resp = $recaptcha->verify($response,$remoteip);
+
+        if($resp->isSuccess())
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     *  Een statuspagina met ram,cpu en hdd usage
+     *  @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     *  @throws \App\Exceptions\CloudAtCostLoginFailException
+     */
+    public function status()
+    {
+        $vps = new CloudAtCost(env("CAC_KEY"), env("CAC_LOGIN"));
+        $vps->selectServer("ovde.be");
+
+        return view("paginas.status",compact("vps"));
+    }
+}
