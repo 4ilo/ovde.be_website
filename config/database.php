a8aa9c98a915bf1566b8ee74839b5c24748718ad
Olivier <oliviervandeneede@hotmail.com>
2017-07-29 11:39:58 +0200

    install redis

--- config/database.php
+++ config/database.php
@@ -111,14 +111,15 @@ return [
     |
     */
 
+
     'redis' => [
 
-        'cluster' => false,
+        'client' => 'predis',
 
         'default' => [
-            'host'     => env('REDIS_HOST', 'localhost'),
+            'host' => env('REDIS_HOST', 'localhost'),
             'password' => env('REDIS_PASSWORD', null),
-            'port'     => env('REDIS_PORT', 6379),
+            'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
 
